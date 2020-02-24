dd9ae9773626d53f14ad0c6331dd4c22ea39cab7
hongleileilei <39015103+hongleileilei@users.noreply.github.com>
2020-02-23 21:14:28 -0500

    Final version of Phase 1

--- main_ver_1.cpp
+++ main_ver_1.cpp
@@ -590,44 +590,45 @@ SC_MODULE(DP){
     dm_wr = CTRL.read().range(7, 7);
     dm_rd = CTRL.read().range(6, 6);
     alu_ctrl = CTRL.read().range(5, 0);
-        // ADDI SUBI ANDI ORI XORI MOVI LSHI ASHI LUI
-        if (alu_ctrl == sc_uint<6>(2) || alu_ctrl == sc_uint<6>(4) || alu_ctrl == sc_uint<6>(8) || alu_ctrl == sc_uint<6>(10) || alu_ctrl == sc_uint<6>(12) || alu_ctrl == sc_uint<6>(14) || alu_ctrl == sc_uint<6>(16) || alu_ctrl == sc_uint<6>(18) || alu_ctrl == sc_uint<6>(19) ) {
-		rf_Wdata.write( alu_result.read() );
- 	}
+    // immediate value operation
+    // ADDI SUBI ANDI ORI XORI MOVI LSHI ASHI LUI
+    if (alu_ctrl == sc_uint<6>(2) || alu_ctrl == sc_uint<6>(4) || alu_ctrl == sc_uint<6>(8) || alu_ctrl == sc_uint<6>(10) || alu_ctrl == sc_uint<6>(12) || alu_ctrl == sc_uint<6>(14) || alu_ctrl == sc_uint<6>(16) || alu_ctrl == sc_uint<6>(18) || alu_ctrl == sc_uint<6>(19) ) {
+      rf_Wdata.write( alu_result.read() );
+ 	  }
 
-
-        // ADD SUB AND OR XOR MOV LSH ASH
-        else if (alu_ctrl == sc_uint<6>(1) || alu_ctrl == sc_uint<6>(3) || alu_ctrl== sc_uint<6>(7) || alu_ctrl == sc_uint<6>(9) || alu_ctrl == sc_uint<6>(11) || alu_ctrl == sc_uint<6>(13) || alu_ctrl == sc_uint<6>(15) || alu_ctrl == sc_uint<6>(17) ){
-		rf_Wdata.write( alu_result.read() );
-	}
-        // LOAD
-	else if (alu_ctrl == sc_uint<6>(20) ) {
-		dm_addr.write( alu_result.read() );
-		rf_Wdata.write( dm_dataout.read() );		
-	}
-        // STOR alu输出连接dm的数据线 rf输出连接dm地址线
-        else if (alu_ctrl == sc_uint<6>(21) ) {
-		dm_datain.write( alu_result.read() );
-		dm_addr.write( rf_Raddr1.read() );
-	}
-        // CMP
-        else if (alu_ctrl == sc_uint<6>(5) ) {
-		; // wr, rd 全零所以无所谓
-	}
-        // CMPI
-        else if (alu_ctrl == sc_uint<6>(6) ) {
-		;
-	}
-        // JUMP 没过alu
-        else if (alu_ctrl == sc_uint<6>(30) || alu_ctrl == sc_uint<6>(31) || alu_ctrl == sc_uint<6>(32) || alu_ctrl == sc_uint<6>(33) || alu_ctrl == sc_uint<6>(34) || alu_ctrl == sc_uint<6>(35) || alu_ctrl == sc_uint<6>(36) || alu_ctrl == sc_uint<6>(37) ) {
-		RTAR.write( rf_data2.read() );
-	}
-        // JAL 没过alu
-        else if (alu_ctrl == sc_uint<6>(38) ) {
-		rf_Wdata.write( PC.read() );
-		RTAR.write( rf_data2.read() );
-	}
-
+    // regular value operation
+    // ADD SUB AND OR XOR MOV LSH ASH
+    else if (alu_ctrl == sc_uint<6>(1) || alu_ctrl == sc_uint<6>(3) || alu_ctrl== sc_uint<6>(7) || alu_ctrl == sc_uint<6>(9) || alu_ctrl == sc_uint<6>(11) || alu_ctrl == sc_uint<6>(13) || alu_ctrl == sc_uint<6>(15) || alu_ctrl == sc_uint<6>(17) ){
+      rf_Wdata.write( alu_result.read() );
+	  }
+    //
+    // LOAD
+	  else if (alu_ctrl == sc_uint<6>(20) ) {
+      dm_addr.write( alu_result.read() );
+		  rf_Wdata.write( dm_dataout.read() );
+	  }
+    // STOR operation, ALU output connected to dm, rf output connected to dm address
+    else if (alu_ctrl == sc_uint<6>(21) ) {
+		  dm_datain.write( alu_result.read() );
+		  dm_addr.write( rf_Raddr1.read() );
+	  }
+    // CMP oparations, wr, rd are all 0s.
+    else if (alu_ctrl == sc_uint<6>(5) ) {
+		  ;
+	  }
+    // CMPI
+    else if (alu_ctrl == sc_uint<6>(6) ) {
+		  ;
+	  }
+    // JUMP condition, which does not pass ALU
+    else if (alu_ctrl == sc_uint<6>(30) || alu_ctrl == sc_uint<6>(31) || alu_ctrl == sc_uint<6>(32) || alu_ctrl == sc_uint<6>(33) || alu_ctrl == sc_uint<6>(34) || alu_ctrl == sc_uint<6>(35) || alu_ctrl == sc_uint<6>(36) || alu_ctrl == sc_uint<6>(37) ) {
+      RTAR.write( rf_data2.read() );
+	  }
+    //JAL operation, which does not pass ALU
+    else if (alu_ctrl == sc_uint<6>(38) ) {
+		  rf_Wdata.write( PC.read() );
+		  RTAR.write( rf_data2.read() );
+	  }
 }
 
 
@@ -1248,7 +1249,7 @@ SC_MODULE(CTRL){
 };
 
 int sc_main(int argc, char* argv[]){
-  sc_signal<sc_uint<16> > instr; 
+  sc_signal<sc_uint<16> > instr;
   sc_signal<sc_uint<4> > psr;
   sc_signal<sc_uint<16> > rtar;
   sc_signal<sc_uint<16> > pc;
@@ -1268,4 +1269,3 @@ int sc_main(int argc, char* argv[]){
 
   return(0);
 }
-
